e8ab674ff3fbada20512465a4dcfb672c7aeab77
Pavel Pozdnyakov <pvplpt@inbox.ru>
2023-07-22 14:41:37 +0500

    added lesson 2

--- group4618/pozdnyakov/MemuOOP.java
+++ group4618/pozdnyakov/MemuOOP.java
@@ -1,13 +1,16 @@
 package group4618.pozdnyakov;
 
 import group4618.pozdnyakov.lesson1.exercise1.Program11;
+import group4618.pozdnyakov.lesson2.exercise1.Program21;
 
 public class MemuOOP {
     public static void main(String[] args) {
         boolean flag = true;
         while (flag) {
             System.out.println("\n\n--- Урок 1. Принципы ООП: Инкапсуляция, наследование, полиморфизм ---");
-            System.out.println("1. Задание 1. Класс ГорячийНапиток и ГорячихНапитковАвтомат");   
+            System.out.println("1. Задание 1. Класс ГорячийНапиток и ГорячихНапитковАвтомат");  
+            System.out.println("--- Урок 2. Принципы ООП Абстракция и интерфейсы. Пример проектирования ---");
+            System.out.println("2. Задание 1. Класс Market4");              
             System.out.println("---");             
             System.out.println("0. Завершение работы приложения");           
             int num = MyTools.inputPositiveNumber("Введите номер задания: ");      
@@ -16,6 +19,9 @@ public class MemuOOP {
                 case 1:
                     Program11.task1();
                     break;  
+                case 2:
+                    Program21.task1();
+                    break;                     
                 case 0:
                     flag = false;
                     System.out.println("\nДо новых встреч!");
---
+++ group4618/pozdnyakov/lesson2/exercise1/Program21.java
@@ -0,0 +1,17 @@
+/*
+ * Реализовать класс Market и все методы, которые он обязан реализовывать.
+ * Методы из интерфейса QueueBehaviour, имитируют работу очереди, 
+ * MarketBehaviour – помещает и удаляет человека из очереди, 
+ * метод update – обновляет состояние магазина (принимает и отдаёт заказы)
+ */
+package group4618.pozdnyakov.lesson2.exercise1;
+
+public class Program21 {
+    public static void main(String[] args) {
+        task1();
+    }
+
+    public static void task1() {
+
+    }
+}
